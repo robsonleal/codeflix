7c79f0c18dfff2a38ad58d44a9445a00713fe99a
Robson Leal <devleal@hotmail.com>
2022-04-10 12:28:55 -0300

    Criando serviço para login a API recuperando um token JWT

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../service/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form!: FormGroup
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private loginService: LoginService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: '',
+      password: ''
+    });
   }
 
+  submit(): void {
+    this.loginService.login(this.form.getRawValue())
+  }
 }
