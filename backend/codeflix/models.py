e365f294691b5024679cb242b9882abc63a047bd
Robson Leal <devleal@hotmail.com>
2022-04-12 08:02:28 -0300

    Fazendo alteracoes para melhorar exibicao dos dados das models

--- backend/codeflix/models.py
+++ backend/codeflix/models.py
@@ -1,10 +1,16 @@
+from pydoc import describe
 from django.db import models
 
 
 class Module(models.Model):
     name = models.CharField(
         max_length=255,
-        verbose_name='Módulo'
+        verbose_name='Nome do módulo'
+    )
+
+    description = models.CharField(
+        max_length=1500,
+        verbose_name='Descrição'
     )
 
     def __str__(self):
@@ -14,15 +20,27 @@ class Module(models.Model):
 class Class(models.Model):
     name = models.CharField(
         max_length=255,
-        verbose_name='Módulo'
+        verbose_name='Nome da aula'
     )
 
-    book = models.ManyToManyField(
+    module = models.ForeignKey(
         Module,
         verbose_name='Módulo',
+        on_delete=models.DO_NOTHING
     )
 
-    date_class = models.DateField()
+    description = models.CharField(
+        max_length=1500,
+        verbose_name='Descrição'
+    )
+
+    date_class = models.DateField(
+        verbose_name='Data da aula'
+    )
+
+    duration = models.IntegerField(
+        verbose_name='Duração'
+    )
 
     def __str__(self):
         return self.name
