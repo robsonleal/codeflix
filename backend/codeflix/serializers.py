8b66ea08046de7e61f24c1abb60b03b0dedc199e
Robson Leal <devleal@hotmail.com>
2022-04-14 14:15:01 -0300

    Alterações finais no backend

--- backend/codeflix/serializers.py
+++ backend/codeflix/serializers.py
@@ -2,19 +2,13 @@ from rest_framework import serializers
 from .models import Class, Module
 
 
-class ModuleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Module
-        fields = '__all__'
-
-
 class ClassSerializer(serializers.ModelSerializer):
     class Meta:
         model = Class
         fields = '__all__'
 
 
-class ClassByModulesSerializer(serializers.ModelSerializer):
+class ModuleSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Class
+        model = Module
         fields = '__all__'
--- backend/codeflix/views.py
+++ backend/codeflix/views.py
@@ -1,24 +1,23 @@
 from rest_framework import viewsets, generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from codeflix.models import Module, Class
-from codeflix.serializers\
-    import ClassSerializer, ModuleSerializer, ClassByModulesSerializer
+from codeflix.serializers import ClassSerializer, ModuleSerializer
 
 
 class ModuleViewSet(viewsets.ModelViewSet):
     queryset = Module.objects.all().order_by('name')
     serializer_class = ModuleSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class ClassViewSet(viewsets.ModelViewSet):
     queryset = Class.objects.all()
     serializer_class = ClassSerializer
-    permission_classes = (AllowAny,)
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class ListClassByModules(generics.ListAPIView):
     def get_queryset(self):
-        return Class.objects.filter(module=self.kwargs['pk'])
-    serializer_class = ClassByModulesSerializer
-    permission_classes = (AllowAny,)
+        return Class.objects.filter(module=self.kwargs['pk']).order_by('name')
+    serializer_class = ClassSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
